75c5ecb1657b1d83f03be8a74ce882eaaa5eea55
Aryan753 <baliyan892329@gmail.com>
2025-12-14 11:19:44 +0530

    color-picker

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const colors=["Red","Green","Black","Yellow","Purple"];
+
+  const handleColor=(color)=>{
+    setBackgroundColor(color);
+  }
+
+  return (
+    <div  style={{backgroundColor:backgroundColor}}> 
+    <h1>Color Picker</h1>
+      <div className='color-number'>
+           {colors.map((color,index)=>(
+            <div
+            key={index}
+            className='color-box'
+            style={{backgroundColor:color}}
+            onClick={()=>handleColor(color)}
+            ></div>
+           ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
